e20c256b67f5a640fe471436df74736d6673c0b2
Justin Cheema <justincheema@quirklogic.com>
2023-02-20 13:51:41 -0700

    Improve_BeerItem - Added star rating system and added image for beer in BeerItem

--- src/components/BeerItem.vue
+++ src/components/BeerItem.vue
@@ -1,23 +1,31 @@
 <template>
   <div class="beer-item">
 
-    <div class="beer-item__content_wrapper">
+    <div class="beer-item__wrapper">
 
-      <div class="beer-item__name_label">
-        <label class="beer-item__name">Name:</label>
-        <span id=beer-item__name_text>{{ name }}</span>
+      <div class="beer-item__icon text-center">
+        <img :src="image_url" class="img-thumbnail" />
       </div>
-      <div class="beer-item__description">
-        <label class="beer-item__description_label">Description:</label>
-        <span id=beer-item__description_text>{{ description }}</span>
-      </div>
-      <div class="beer-item__first_brewed">
-        <label class="beer-item__first_brewed_label">First Brewed:</label>
-        <span id=beer-item__first_brewed_text>{{ first_brewed }}</span>
-      </div>
-      <div class="beer-item__food_pairing">
+
+      <div class="beer-item__content_wrapper">
+
+        <div class="beer-item__name_label">
+          <label class="beer-item__name">Name:</label>
+          <span id=beer-item__name_text>{{ name }}</span>
+        </div>
+        <div class="beer-item__description">
+          <label class="beer-item__description_label">Description:</label>
+          <span id=beer-item__description_text>{{ description }}</span>
+        </div>
+        <div class="beer-item__first_brewed">
+          <label class="beer-item__first_brewed_label">First Brewed:</label>
+          <span id=beer-item__first_brewed_text>{{ first_brewed }}</span>
+        </div>
         <label class="beer-item__food_pairing_label">Food Pairings:</label>
-        <span id=beer-item__food_pairing_text>{{ food_pairing }}</span>
+        <div class="beer-item__food_pairing" v-for="(pairing, index) in food_pairings" :key="index">
+          <span id=beer-item__food_pairing_text>{{ pairing }}</span>
+        </div>
+
       </div>
 
     </div>
@@ -27,18 +35,13 @@
       <form class="beer-item__form" @submit.prevent>
 
         <label class="beer-item__select_label" for="beer-item__select">How would you rate this beer?</label>
-        <div class="beer-item__select" id="beer-item__select">
-          <label for="one">1</label>
-          <input type="radio" id="one" value="1" v-model.number="state.selected" @focus="resetState" />
-          <label for="two">2</label>
-          <input type="radio" id="two" value="2" v-model.number="state.selected" @focus="resetState" />
-          <label for="three">3</label>
-          <input type="radio" id="three" value="3" v-model.number="state.selected" @focus="resetState" />
-          <label for="four">4</label>
-          <input type="radio" id="four" value="4" v-model.number="state.selected" @focus="resetState" />
-          <label for="five">5</label>
-          <input type="radio" id="five" value="5" v-model.number="state.selected" @focus="resetState" />
-        </div>
+
+        <star-rating class="beer-item__select"
+            @update:rating ="setRating"
+            v-bind:star-size="25"
+            v-bind:padding="5"
+            v-bind:show-rating="false"
+        />
 
         <label class="beer-item__comments_label" for="beer-item__textarea">Add a comment here:</label>
         <div class="beer-item__comments">
@@ -46,7 +49,7 @@
         </div>
 
         <button
-          @click="createRating(id, state.comments, state.selected)"
+          @click="createRating(id, state.comments)"
           :disabled="state.disableButton"
         >
           Submit Rating
@@ -68,8 +71,10 @@
 
 <script>
   import { useAuthStore } from '@/store/auth'
-  import BeerRequests from './../api/beer_api_requests'
   import { reactive } from 'vue'
+  import BeerRequests from './../api/beer_api_requests'
+  import StarRating from 'vue-star-rating'
+
   export default {
     name: 'BeerItem',
     props: {
@@ -89,14 +94,21 @@
         type: String,
         required: true
       },
-      food_pairing: {
+      food_pairings: {
         type: Array,
+        required: false
+      },
+      image_url: {
+        type: String,
         required: false
       }
     },
+    components: {
+      StarRating
+    },
     setup() {
       const state = reactive({
-        selected: 5,
+        rating: 0,
         comments: '',
         disableButton: false,
         error: false,
@@ -108,13 +120,12 @@
        *
        * @param id - Id of the beer being rated
        * @param comments - Optional parameter for any comments that should be added to the rating
-       * @param rating - Rating given. Must be a number between 1 and 5 inclusive
        * @returns {Promise<void>}
        */
-      const createRating = async (id, comments, rating) => {
+      const createRating = async (id, comments) => {
         // Prevent double clicks and duplicate requests
         state.disableButton = true
-        const body = { rating: rating, comments: comments }
+        const body = { rating: state.rating, comments: comments }
         try {
           const authStore = useAuthStore()
           await BeerRequests.createRating(authStore?.user?.email, id, body)
@@ -131,6 +142,15 @@
       }
 
       /**
+       * Sets the rating for this item when the rating component is clicked
+       *
+       * @param rating
+       */
+      const setRating = (rating) => {
+        state.rating = rating
+      }
+
+      /**
        * This function will disable any error or success states on the page.
        */
       const resetState = () => {
@@ -141,6 +161,7 @@
       return {
         state,
         createRating,
+        setRating,
         resetState
       }
     }
@@ -159,10 +180,24 @@
     cursor: pointer;
     transition: all 0.25s ease;
 
-    .beer-item__content_wrapper {
-      label {
-        margin-right: 1em;
-        font-weight: bold;
+    .beer-item__wrapper {
+      display: grid;
+      grid-template-columns: 10% 90%;
+
+      .beer-item__icon {
+        margin-top: auto;
+        margin-bottom: auto;
+        img {
+          height: 175px;
+        }
+      }
+
+      .beer-item__content_wrapper {
+        margin-left: 1em;
+        label {
+          margin-right: 1em;
+          font-weight: bold;
+        }
       }
     }
 
@@ -176,15 +211,9 @@
         font-size: 1em;
       }
 
-      .beer-item__select {
+      .vue-star-rating.beer-item__select {
         margin-top: 0.2em;
         margin-bottom: 1em;
-        label {
-          margin-right: 0.1em;
-        }
-        input {
-          margin-right: 2em;
-        }
       }
 
       .beer-item__comments {
